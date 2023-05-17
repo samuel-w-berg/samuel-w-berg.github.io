fc19b8aa09ef8b65061dfff3f5b7f6ac63b55715
samuel-w-berg <samuel.w.berg@gmail.com>
2023-05-17 14:58:12 -0500

    Add components and style menu

---
+++ src/ContactMe/ContactMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactMe() {
+    return (
+        <section>
+            <h2>Contact Me</h2>
+            <p>Email: example@example.com</p>
+            <p>Phone: 123-456-7890</p>
+            <p>Address: 123 Main St, Anytown USA</p>
+        </section>
+    );
+}
+
+export default ContactMe;
