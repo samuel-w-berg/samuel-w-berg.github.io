051dfa29170579db349ea428608f15a5362be6bb
samuel-w-berg <samuel.w.berg@gmail.com>
2023-05-21 20:00:06 -0500

    Complete Portfolio

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 
 export default function Portfolio() {
     return (
@@ -7,17 +7,19 @@ export default function Portfolio() {
             <h2>My Projects</h2>
             <Card.Group>
                 <Card>
+                    <Image src='https://i.imgur.com/DkcQ2rE.png' wrapped ui={false} />
                     <Card.Content>
                         <Card.Header>Connect Four</Card.Header>
                         <Card.Description>
-                            A classic game of Connect Four built with React.
+                            A classic game of Connect Four built with JavaScript.
                         </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
-                        <a href="#">View on GitHub</a>
+                        <a href="https://github.com/samuel-w-berg/connect-4">View on GitHub</a>
                     </Card.Content>
                 </Card>
                 <Card>
+                    <Image src='https://i.imgur.com/wQeJbMH.png' wrapped ui={false} />
                     <Card.Content>
                         <Card.Header>Java-Ripped</Card.Header>
                         <Card.Description>
@@ -25,10 +27,11 @@ export default function Portfolio() {
                         </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
-                        <a href="#">View on GitHub</a>
+                        <a href="https://github.com/samuel-w-berg/Java-Ripped">View on GitHub</a>
                     </Card.Content>
                 </Card>
                 <Card>
+                    <Image src='https://i.imgur.com/xFQaCL6.png' wrapped ui={false} />
                     <Card.Content>
                         <Card.Header>BarFly</Card.Header>
                         <Card.Description>
@@ -36,7 +39,19 @@ export default function Portfolio() {
                         </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
-                        <a href="#">View on GitHub</a>
+                        <a href="https://github.com/samuel-w-berg/BarFly">View on GitHub</a>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Image src='https://i.imgur.com/fMLQra7.png' wrapped ui={false} />
+                    <Card.Content>
+                        <Card.Header>BS Job Hunter</Card.Header>
+                        <Card.Description>
+                            A Job Search app built with MongoDB, Express, React, and Node.js.
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a href="https://github.com/samuel-w-berg/Job-Tracker-WIP">View on GitHub</a>
                     </Card.Content>
                 </Card>
             </Card.Group>
